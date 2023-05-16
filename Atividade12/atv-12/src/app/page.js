5285305cdd9fc077398f8d3d015c08b6071cd389
Aurelio <aurelionadjabarreto@gmail.com>
2023-05-16 10:02:24 -0300

    Update package-lock.json, package.json, and page.js

--- Atividade12/atv-12/src/app/page.js
+++ Atividade12/atv-12/src/app/page.js
@@ -1,43 +1,10 @@
-"use client";
-
-import styles from "./page.module.css";
-import React from "react";
-import { useState } from "react";
-
-function SearchBar({ filterText, onFilterTextChange }) {
-  return (
-    <form>
-      <label>Procure uma mensagem:</label>
-      <input
-        type="text"
-        value={filterText}
-        placeholder="Search..."
-        onChange={(e) => onFilterTextChange(e.target.value)}
-      />
-    </form>
-  );
-}
-
-function FilterableMessageTable({ messages }) {
-  const [filterText, setFilterText] = useState("");
-
-  return (
-    <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
-  );
-}
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
-  const [blogMessages, setBlogMessages] = useState([]);
-
-  fetch("https://...")
-    .then((response) => response.json())
-    .then((data) => {
-      setBlogMessages(data);
-    });
-
   return (
     <main className={styles.main}>
-      <FilterableMessageTable messages={blogMessages} />
+      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
     </main>
-  );
+  )
 }
