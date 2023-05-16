492f9d5687bbd868376bd09cae0111c06d10b96a
Aurelio <aurelionadjabarreto@gmail.com>
2023-05-16 10:36:00 -0300

    Update page.js

--- Atividade12/atv-12/src/app/page.js
+++ Atividade12/atv-12/src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import styles from './page.module.css'
 import { useState } from "react";
 
@@ -29,7 +28,7 @@ function FilterableMessageTable({ messages }) {
 export default function Home() {
   const [blogMessages, setBlogMessages] = useState([]);
 
-  fetch("https://...")
+  fetch("https://script.google.com/macros/s/AKfycbzBn3sALe1rYjz7Ze-Ik7q9TEVP0I2V3XX7GNcecWP8NvCzGt4yO_RT1OlQp09TE9cU/exec")
     .then((response) => response.json())
     .then((data) => {
       setBlogMessages(data);
