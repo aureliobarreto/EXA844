98f6514e50429fae28e33e5dac93d797e32b5e89
Aurelio <aurelionadjabarreto@gmail.com>
2023-05-16 09:50:25 -0300

    Update page.js

--- Atividade12/atv-12/src/app/page.js
+++ Atividade12/atv-12/src/app/page.js
@@ -1,43 +1,94 @@
-"use client";
-
-import styles from "./page.module.css";
-import React from "react";
-import { useState } from "react";
-
-function SearchBar({ filterText, onFilterTextChange }) {
-  return (
-    <form>
-      <label>Procure uma mensagem:</label>
-      <input
-        type="text"
-        value={filterText}
-        placeholder="Search..."
-        onChange={(e) => onFilterTextChange(e.target.value)}
-      />
-    </form>
-  );
-}
-
-function FilterableMessageTable({ messages }) {
-  const [filterText, setFilterText] = useState("");
-
-  return (
-    <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
-  );
-}
+import styles from './page.module.css'
 
 export default function Home() {
-  const [blogMessages, setBlogMessages] = useState([]);
-
-  fetch("https://...")
-    .then((response) => response.json())
-    .then((data) => {
-      setBlogMessages(data);
-    });
-
   return (
     <main className={styles.main}>
-      <FilterableMessageTable messages={blogMessages} />
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
-  );
+  )
 }
